bc150ddaa5f0b59361fe16b167ed4c34d2a12906
Akash <akashr6514@gmail.com>
2025-01-20 21:22:17 +0530

    Testing Phone Auth firebase

--- Frontend/src/firebase/reCaptcha.js
+++ Frontend/src/firebase/reCaptcha.js
@@ -6,8 +6,12 @@ const Auth = getAuth(app);
 const initializeRecaptcha = async (auth = Auth, containerId) => {
     return new Promise((resolve, reject) => {
         try {
+            if (window.recaptchaVerifier) {
+                window.recaptchaVerifier.clear();
+            }
+
             window.recaptchaVerifier = new RecaptchaVerifier(
-                containerId,
+                auth, containerId,
                 {
                     size: "invisible",
                     callback: (response) => {
@@ -18,8 +22,8 @@ const initializeRecaptcha = async (auth = Auth, containerId) => {
                         console.warn("reCAPTCHA expired. Please try again.");
                         reject("reCAPTCHA expired.");
                     },
-                },
-                auth
+                }
+                
             );
 
             window.recaptchaVerifier.render().then(() => resolve());
