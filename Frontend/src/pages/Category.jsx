0ac8750b623cab18ea210a7b5cfd3f929e8682ce
akashr206 <akashr6514@gmail.com>
2025-05-04 17:10:29 +0530

    fix typo

--- Frontend/src/pages/Category.jsx
+++ Frontend/src/pages/Category.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Separator } from "../components/ui/Separator";
+import { Separator } from "../components/ui/separator";
 import Loading from "../components/Loading";
 import ProductsGrid from "../components/ProductsGrid";
 import FilterAndSort from "../components/FilterAndSort";
